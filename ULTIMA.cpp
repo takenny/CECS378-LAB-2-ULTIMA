86a86e18171d553c177309729d2081e7f128701d
Kenny Ta <kenny_ta@yahoo.com>
2020-11-02 12:14:01 -0800

    update Ultima app
    pref update / cpp file update with more code.

--- ULTIMA.cpp
+++ ULTIMA.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 using namespace std;
-int main() 
+int main()
 {
+    cout << "HELLO WELCOME TO THE ULTIMA CHEATS ENGINE" << endl;
+    cout << "What would you like to edit?" << endl;
+    
     return 0;
 }
